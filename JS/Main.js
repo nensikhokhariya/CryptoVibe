263d7d5519ca2011f16c52239a61e61167cd6c06
nensikhokhariya <nensipatel13310@gmail.com>
2024-09-25 10:40:43 +0000

    crypto

--- JS/Main.js
+++ JS/Main.js
@@ -24,11 +24,25 @@ $(document).ready(function() {
   });
 
   // Change background color when scrolling past 50px
+  
+});
+
+
+$(document).ready(function() {
+  var $header = $('header'); // Update selector to target your header element
+  
   $(window).on('scroll', function() {
     if ($(this).scrollTop() > 50) {
-      $header.css('background-color', '#000618');
+      $header.css({
+        'background-color': 'rgba(43, 101, 150, 1)',
+        'padding-top': '20px',  // Adjust top padding
+        'padding-bottom': '20px'  // Adjust bottom padding
+      });
     } else {
-      $header.css('background-color', 'transparent');
+      $header.css({
+        'background-color': 'transparent'
+      });
     }
   });
 });
+
