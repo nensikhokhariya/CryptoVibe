89af25438b45d27975323892db9b33752c81f5eb
nensikhokhariya <nensipatel13310@gmail.com>
2024-10-01 12:39:16 +0000

    crypto

--- JS/Main.js
+++ JS/Main.js
@@ -51,3 +51,51 @@ $(document).ready(function(){
   $('.advantage-section .inner-advantage-block').matchHeight(); 
   
 })
+
+
+
+
+// slider
+
+
+// ressponsive slider 3
+
+
+$('.team-slide').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  autoplay:true,
+  autoplaySpeed:2000,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+	
